eb3a801a6cb7ec6fe0f6798042608db5d96f337c
lexvonghost <lex_von_tost@mail.ru>
2022-09-19 20:19:47 +0300

    feat: реализовал алгоритм

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 из строк, длина которых меньше либо равна трём символам.*/
 
 string[] input = new string[] {"12343", ":-)", "world", "Russia", "-2"};
+string[] output = new string[5];
+int outputIndex = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        output[outputIndex] = input[i];
+        outputIndex++;
+    }
+}
